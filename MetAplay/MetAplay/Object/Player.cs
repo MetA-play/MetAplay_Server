1f568681769f756c8e6c3d52e138dc5f7f78a1ee
hsh0517 <pelino0517@gmail.com>
2022-12-05 17:37:16 +0900

    Update namespace

--- MetAplay/MetAplay/Object/Player.cs
+++ MetAplay/MetAplay/Object/Player.cs
@@ -1,5 +1,4 @@
 ﻿using Google.Protobuf.Protocol;
-using Server.Game;
 using Server;
 using System;
 using System.Collections.Generic;
@@ -7,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MetAplay.Object
+namespace MetAplay
 {
     public class Player : GameObject
     {
