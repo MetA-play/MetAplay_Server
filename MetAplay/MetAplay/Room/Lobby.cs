5ffa0bd39ac9651af3c801965594e1411dbd0f36
정은성 <dmstjd0911@naver.com>
2022-12-05 19:39:51 +0900

    ADD EnterGame

--- MetAplay/MetAplay/Room/Lobby.cs
+++ MetAplay/MetAplay/Room/Lobby.cs
@@ -9,12 +9,41 @@ namespace MetAplay
 {
     public class Lobby : BaseRoom
     {
-        public Dictionary<int, Player> _players = new Dictionary<int, Player>();
 
         public override void EnterGame(GameObject gameObject)
         {
-            S_EnterGame enterGame = new S_EnterGame();
 
+            if(gameObject.ObjectType == GameObjectType.Player)
+            {
+                Player player = gameObject as Player;
+                _players.Add(gameObject.Id,player);
+
+                {
+                    S_EnterGame entergame = new S_EnterGame();
+                    player.Session.Send(entergame);
+                    S_Spawn spawn = new S_Spawn();
+
+                    foreach (Player p in _players.Values)
+                    {
+                        if(player != p)
+                            spawn.Objects.Add(p.Info);
+                    }
+
+                    player.Session.Send(spawn);
+                }
+            }
+
+            {
+                S_Spawn spawn = new S_Spawn();
+
+                spawn.Objects.Add(gameObject.Info);
+
+                foreach (Player p in _players.Values)
+                {
+                    if (p.Id != gameObject.Id)
+                        p.Session.Send(spawn);
+                }
+            }
 
         }
     }
