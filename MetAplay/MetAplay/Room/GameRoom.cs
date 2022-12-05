cc07b1761e2ab2c19ae0037d9700982df7d7b96a
hsh0517 <pelino0517@gmail.com>
2022-12-05 20:46:18 +0900

    ADD EnterGame, LeaveGame function

--- MetAplay/MetAplay/Room/GameRoom.cs
+++ MetAplay/MetAplay/Room/GameRoom.cs
@@ -1,13 +1,63 @@
-﻿using MetAplay;
+﻿using Google.Protobuf.Protocol;
+using MetAplay;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class GameRoom : BaseRoom
+namespace MetAplay
 {
+    public class GameRoom : BaseRoom
+    {
+        public override void EnterGame(GameObject gameObject)
+        {
+            if (gameObject is null) return;
+            if (gameObject is not Player player) return;
 
-    
+            _players.Add(player.Id, player);
+            player.Room = this;
 
+            // 본인에게 전송
+            {
+                S_EnterGame enterGamePacket = new S_EnterGame();
+                enterGamePacket.Player = player.Info;
+                player.Session.Send(enterGamePacket);
+
+                S_Spawn spawnPacket = new S_Spawn();
+                foreach (Player p in _players.Values)
+                    if (p != player)
+                        spawnPacket.Objects.Add(p.Info);
+                player.Session.Send(spawnPacket);
+            }
+
+            // 타인에게 전송
+            {
+                S_Spawn spawnPacket = new S_Spawn();
+                foreach (Player p in _players.Values)
+                    if (p.Id != player.Id) p.Session.Send(spawnPacket);
+            }
+        }
+
+        public override void LeaveGame(int gameObjectId)
+        {
+            if (!_players.Remove(gameObjectId, out Player player)) return;
+
+            player.Room = null;
+
+            // 본인에게 전송
+            {
+                S_LeaveGame leaveGamePacket = new S_LeaveGame();
+                player.Session.Send(leaveGamePacket);
+            }
+
+            // 타인에게 전송
+            {
+                S_Despawn despawnPacket = new S_Despawn();
+                despawnPacket.ObjectId.Add(gameObjectId);
+                foreach (Player p in _players.Values)
+                    if (p.Id != player.Id) p.Session.Send(despawnPacket);
+            }
+        }
+    }
 }
