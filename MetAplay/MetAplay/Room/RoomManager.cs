5c85176a8aa9fecddfd19759d678dd1657f452c1
정은성 <dmstjd0911@naver.com>
2022-12-05 22:56:42 +0900

    UPDATE,ADD Packet, RoomManager

---
+++ MetAplay/MetAplay/Room/RoomManager.cs
@@ -0,0 +1,74 @@
+﻿using Google.Protobuf.Protocol;
+
+
+namespace MetAplay
+{
+    public class RoomManager
+    {
+        public static RoomManager Instance { get; } = new RoomManager();
+
+        object _lock = new object();
+        Dictionary<int, GameRoom> _rooms = new Dictionary<int, GameRoom>();
+
+        public List<GameRoom> Rooms
+        {
+            get
+            {
+                List<GameRoom> list = new List<GameRoom>();
+                foreach (GameRoom r in _rooms.Values)
+                {
+                    list.Add(r);
+                }
+                return list;
+            }
+        }
+        public int Count { get { return _rooms.Count; } }
+        int _roomId = 1;
+
+        public GameRoom Add(RoomSetting setting)
+        {
+            GameRoom gameRoom = new GameRoom();
+
+            lock (_lock)
+            {
+                gameRoom.RoomId = _roomId;
+                //gameRoom.Setting = setting;
+                _rooms.Add(_roomId, gameRoom);
+                _roomId++;
+            }
+
+            return gameRoom;
+        }
+
+        public bool Remove(int roomId)
+        {
+            lock (_lock)
+            {
+                return _rooms.Remove(roomId);
+            }
+        }
+
+        public GameRoom Find(int roomId)
+        {
+            lock (_lock)
+            {
+                GameRoom room = null;
+                if (_rooms.TryGetValue(roomId, out room))
+                    return room;
+
+                return null;
+            }
+        }
+
+        /*public List<RoomInfo> LoadRoomInfo()
+        {
+            List<RoomInfo> info = new List<RoomInfo>();
+
+            foreach (GameRoom room in _rooms.Values)
+                info.Add(new RoomInfo() { Id = room.RoomId, CurrentPersonnel = room._sessions.Count, Setting = room.Setting });
+
+
+            return info;
+        }*/
+    }
+}
