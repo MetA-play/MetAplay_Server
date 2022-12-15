d9112357d23125568695c341a30933c764f653c3
hsh0517 <pelino0517@gmail.com>
2022-12-15 18:21:48 +0900

    ADD AvoidLog.cs

---
+++ MetAplay/MetAplay/Room/Game/AvoidLog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetAplay
+{
+    public class AvoidLog : Game
+    {
+        private GameObject _log;
+        private float LogRotY
+        {
+            get { return _log.Info.Transform.Rot.Y; }
+            set { _log.Info.Transform.Rot.Y = value; }
+        }
+
+        private int _speedUpTick = 15000;
+
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            
+            _log.Update();
+        }
+
+        public override void End()
+        {
+            base.End();
+        }
+
+        async private void SpeedUp()
+        {
+            if (!State.Equals(GameState.Start)) return;
+            await Task.Delay(_speedUpTick);
+            // TODO
+        }
+    }
+}
