8e9af7fad65c3ffcf12e114efd03f77a95f2d069
정은성 <dmstjd0911@naver.com>
2022-12-15 17:49:22 +0900

    ADD Game interface

---
+++ MetAplay/MetAplay/Room/Game/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetAplay
+{
+    public interface IGame
+    {
+        public void Init();
+        public void Start();
+        public void Update();
+        public void End();
+    }
+
+    public enum GameState
+    {
+        Ready,
+        Start,
+        End,
+    }
+
+    public class Game : IGame
+    {
+        public GameState State { get; protected set; }
+
+        public virtual void Init()
+        {
+            State = GameState.Ready;
+        }
+
+        public virtual void Start()
+        {
+            State = GameState.Start;
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void End()
+        {
+            State = GameState.End;
+        }
+    }
+}
