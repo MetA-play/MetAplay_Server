1b7400905eb91b9806a00a9a7d76051dd7055927
정은성 <dmstjd0911@naver.com>
2022-12-19 09:12:13 +0900

    UPDATE Protocol

--- Common/protoc-3.12.3-win64/bin/protocolGenerator.bat
+++ Common/protoc-3.12.3-win64/bin/protocolGenerator.bat
@@ -3,4 +3,6 @@ IF ERRORLEVEL 1 PAUSE
 
 START ../../../MetAplay/ProtocolGenerator/bin/Debug/net6.0/ProtocolGenerator.exe ./Protocol.proto
 XCOPY /Y Protocol.cs "../../../MetAplay/MetAplay/Packet"
+XCOPY /Y Protocol.cs "../../../../MetAPlay_Client/Assets/Scripts/Network/Packet"
 XCOPY /Y ServerPacketManager.cs "../../../MetAplay/MetAplay/Packet"
+XCOPY /Y ClientPacketManager.cs "../../../../MetAPlay_Client/Assets/Scripts/Network/Packet"
