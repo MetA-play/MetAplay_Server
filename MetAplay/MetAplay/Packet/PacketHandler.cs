9321965e2e84a13454303004b0dea2b5690f0155
EnSung <dmstjd0911@naver.com>
2022-12-28 09:14:04 +0900

    UPDATE Protocol

--- MetAplay/MetAplay/Packet/PacketHandler.cs
+++ MetAplay/MetAplay/Packet/PacketHandler.cs
@@ -56,4 +56,12 @@ public class PacketHandler
 
         clientSession.MyPlayer.Room.Push(clientSession.MyPlayer.Room.GameStartHandle, clientSession.MyPlayer);
     }
+
+    public static void C_SetUserinfoHandler(PacketSession session, IMessage packet)
+    {
+        ClientSession clientSession = session as ClientSession;
+        C_SetUserinfo info = packet as C_SetUserinfo;
+
+        clientSession.UserData = info.Info;
+    }
 }
